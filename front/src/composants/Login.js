e96d907c67e173a701fcd4e219bc8409fdebf3c0
Ampah26 <akomlassan26@gmail.com>
2024-07-10 22:28:31 +0200

    Liaison page login et signup OK

---
+++ front/src/composants/Login.js
@@ -0,0 +1,95 @@
+import React from "react";
+import Recaptcha from "./recaptcha";
+import Footer from "./Footer";
+import { Link } from 'react-router-dom';
+
+
+function Login () {
+ 
+
+    return (
+        <div>
+            
+            <div class="scrollable-page"  style={{ height: '653px', position: 'relative', overflowY: 'scroll' }}>
+
+                { /* formulaire signIn */ }   
+
+                <div class="form-signin">
+                    <div  class="card bg-white mb15">
+                    <div class="card-header text-center">
+                        <h2>Sign in</h2>
+                    </div>
+
+                    <div class="card-body p30 rounded-bottom">
+                        <form action="" id="signin-form" class="general-form" method="post" accept-charset="utf-8">
+                            <input type='hidden' name="rise_csrf_token" value="bde860d90acd92894b2c7975d0839a63"/>             
+                        <div class="form-group">
+                            <input type="text" name="email" value="" id="email" class="form-control p10" placeholder="Email" autofocus data-rule-required data-msg-required="This field is required." data-rule-email data-msg-email="Please enter a valid email address."/>
+                        </div>
+
+                        <div class="form-group">
+                            <input type="password" name="password" value="" id="password" class="form-control p10" placeholder="Password" data-rule-required data-msg-required="This field is required."/>
+                        </div>
+
+                            <input type='hidden'></input>  
+
+                        <div class="form-group">
+                            <div class="g-recaptcha" data-sitekey="6Le0jrkZAAAAANa3Yq5XfzHC4h34e4K6ynXOhlHK">
+                                <Recaptcha/>
+                            </div>
+                        </div>
+                        <script type="text/javascript"src="https://www.google.com/recaptcha/api.js?hl=en"></script>
+
+                        <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in
+                            <link to="Register"/>
+                        </button>
+
+                        </form>
+
+                        <div class="mt5"><a href="https://rise.fairsketch.com/signin/request_reset_password">Forgot password?</a></div>
+                        <div class="mt20">You don't have an account? &nbsp; <Link to="/signup">Sign up</Link> </div>
+                    </div>
+
+                    </div>
+
+                    <div class="card bg-white mb15">
+                    <div class="card-body p30 rounded-bottom">
+                        <div class="box p10 b-b strong text-center"> 
+                        Sign in as
+                        </div>
+
+                        <div class="box p10 clickable fill-login b-b">
+                        <div class="box-content">Admin</div>
+                        <div class="box-content demo-email">admin@demo.com</div>
+                        <div class="box-content text-right demo-password">riseDemo</div>
+                        </div>
+
+                        <div class="box p10 clickable fill-login b-b">
+                        <div class="box-content">Client</div>
+                        <div class="box-content demo-email">client@demo.com</div>
+                        <div class="box-content text-right demo-password">riseDemo</div>
+                        </div>
+
+                        <div class="box p10">
+                            Identifiant ci dessus
+                        </div>
+                    </div>
+                    </div>
+
+                </div>
+
+
+            </div>
+            
+            
+            
+
+        { /*  Pied de page du formulaire signIn */ }   
+            <Footer/>
+            
+        </div>
+
+    )
+}
+
+export default Login
---
+++ front/src/composants/Register.js
@@ -0,0 +1,115 @@
+import React from "react";
+import Recaptcha from "./recaptcha";
+import Footer from "./Footer";
+import { Link } from 'react-router-dom';
+
+function Register () {
+
+
+    return ( 
+     <div>
+         
+        <div class="scrollable-page"  style={{ height: '653px', position: 'relative', overflowY: 'scroll' }}>
+
+            { /* formulaire signUp */ }   
+
+            <div class="form-signin">
+
+                <div class="card bg-white clearfix"> 
+                   <div class="card-header text-center">
+                        <h2 class="form-signin-heading">Sign up</h2>
+                        <p>Create an account as a new client.</p>
+                    </div>
+
+                    <div class="card-body p30 rounded-bottom">
+                        <form action="" id="signup-form" class="general-form" method="post" accept-charset="utf-8">
+                            <input type='hidden' name="rise_csrf_token" value="bde860d90acd92894b2c7975d0839a63"/>             
+                        <div class="form-group">
+                            <label for="name" class="col-md-12">First name</label>                
+                            <div class="col-md-12">
+                                <input type="text" name="first_name" value="" id="first_name" class="form-control" autofocus="" data-rule-required="" data-msg-required="This field is required."/>
+                            </div>
+                        
+                        </div>
+
+                        <div class="form-group">
+                            <label for="last_name" class="col-md-12">Last name</label>
+                            <div class="col-md-12">
+                                <input type="text" name="last_name" value="" id="last_name" class="form-control" data-rule-required="" data-msg-required="This field is required."/>
+                            </div>
+                        </div>
+                            <input type='hidden'></input>  
+
+                        <div class="form-group">
+                            <div class="row">
+                                <label for="account_type" class="col-md-12">Type</label>
+                                <div class="col-md-12">
+                                    <input type="radio" name="account_type" value="organization" checked="checked" id="type_organization" class="form-check-input account-type" data-msg-required="This field is required."/>
+                                    <label for="type_organization" class="mr15">Organization</label>
+                                    <input type="radio" name="account_type" value="person" id="type_person" class="form-check-input account-type" data-msg-required="This field is required."/>                                    
+                                    <label for="type_person" class="">Individual</label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group company-name-section">
+                            <label for="company_name" class="col-md-12">Company name</label>
+                            <div class="col-md-12">
+                                <input type="text" name="company_name" value="" id="company_name" class="form-control"/>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email" class="col-md-12">Email</label>
+                            <div class="col-md-12">
+                                <input type="text" name="email" value="" id="email" class="form-control" data-rule-email="" data-msg-email="Please enter a valid email address." data-rule-required="" data-msg-required="This field is required."/>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="password" class="col-md-12">Password</label>
+                            <div class="col-md-12">
+                                <input type="password" name="password" value="" id="password"   class="form-control" data-rule-required="" data-msg-required="This field is required." data-rule-minlength="6" data-msg-minlength="Please enter at least 6 characters." autocomplete="off" style={{ zIndex: 'auto' }} />
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="retype_password" class="col-md-12">Retype password</label>
+                            <div class="col-md-12">
+                                <input type="password" name="retype_password" value="" id="retype_password" class="form-control" data-rule-equalto="#password"  autocomplete="off" style={{ zIndex: 'auto' }}  data-msg-equalto="Please enter the same value again."/>
+                            </div>
+                        </div>
+
+
+                        <div class="form-group">
+                            <div class="g-recaptcha" data-sitekey="6Le0jrkZAAAAANa3Yq5XfzHC4h34e4K6ynXOhlHK">
+                                <Recaptcha/>
+                            </div>
+                        </div>
+                        <script type="text/javascript"src="https://www.google.com/recaptcha/api.js?hl=en"></script>
+
+                        <div class="form-group">
+                            <div class="col-md-12">
+                                <button class="w-100 btn btn-lg btn-primary" type="submit">Sign up</button>
+                            </div>
+                        </div>
+
+                        </form>
+
+                    </div>
+                </div>
+                <div className="mt20">You have an account? &nbsp; <Link to="/">Sign in</Link></div>
+                </div>
+        </div>
+
+        
+        
+
+      { /*  Pied de page du formulaire signIn */ }   
+        <Footer/>
+
+    </div>
+
+            
+    )
+}
+
+export default Register
