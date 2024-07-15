325fa2d23adf0c0718a041ab2fe0a1c77e09796e
Ampah26 <akomlassan26@gmail.com>
2024-07-15 19:14:07 +0200

    Ajout de Admin.js

--- front/src/App.js
+++ front/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Login from './composants/Login';
 import Register from './composants/Register';
+import Admin from './composants/Admin';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path='/admin' element={<Admin />}/>
           <Route path="/signup" element={<Register />} />
         </Routes>
       </Router>
